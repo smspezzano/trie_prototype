a61ea98dada4115123967c4ec9a66550aed5e887
smspezzano <smspezzano@gmail.com>
2014-03-09 01:00:40 -0800

    got find to work

--- public/trie.js
+++ public/trie.js
@@ -36,9 +36,11 @@ Trie.prototype.getWords = function(words, currentWord){
   // contained in this Trie.
   // it will use currentWord as a prefix,
   // since a Trie doesn't know about its parents.
-  words = [];
-  if (this.find(currentWord)){
-    words.push(currentWord);
+  words = words || [];
+  var results = this.find(currentWord);
+  for (var i = 0; i < results.length; i++){
+    var word = results[i];
+    words.push(word);
   }
   return words;
 };
@@ -47,19 +49,26 @@ Trie.prototype.find = function(word, index){
   // This function will return the node in the trie
   // which corresponds to the end of the passed in word.
 
+  // don't see why this doesn't work:
+  //  if(this.characters[word[word.length - 1]]){
+  //   return this;
+  // }
+
   // Be sure to consider what happens if the word is not in this Trie.
   index = index || 0;
+  word = word || "";
   //var character = word[index];
 
-  if(this.characters[word[word.length]]){
+  if(index >= word.length){
     return this;
   }
-// word.length <= index
+
   if(this.characters[word[index]] === undefined){
     return false;
-  } 
+  } else {
+    return this.characters[word[index]].find(word, index + 1);
+  }
 
-  return this.characters[word[index]].find(word, index + 1);
 };
 
 Trie.prototype.autoComplete = function(prefix){
